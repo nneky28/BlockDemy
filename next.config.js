32a05196ecaa83b782962164118074c2ed16c59c
Nneka Ezema <nnekaezema@Corporates-MacBook-Pro.local>
2024-03-23 23:16:46 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Add your Next.js configuration options here
+  };
+  
+  module.exports = nextConfig;
+  
